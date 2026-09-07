211e9b8e4572f4e10e195854d839ea220d6400b4
Demi0413 <1519019892@qq.com>
2026-09-07 18:33:21 +0800

    添加主菜单和制作人员面板

---
+++ Assets/QuitGame.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void OnQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
